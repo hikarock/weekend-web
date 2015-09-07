f6552fe6ffc6f59470f4553a7e7841599facac23
hikarock <i@hika69.com>
2015-09-08 00:19:33 +0900

    数字が増えるよ

--- weekend-web.js
+++ weekend-web.js
@@ -1,9 +1,22 @@
+Click = new Mongo.Collection('clicks');
+
 if (Meteor.isClient) {
-  //
+
+  Template.logo.events({
+    'click h1': function() {
+      Click.insert({});
+    }
+  });
+
+  Template.logo.helpers({
+    'count': function() {
+      return Click.find().count();
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    //
   });
 }
